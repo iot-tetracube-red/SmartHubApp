463aa792267ecd4f3bba2fa07b8ff54c7c843ca4
Davide-Di-Do <davide.di.do@icloud.com>
2021-01-23 16:39:15 +0100

    Initial Commit

---
+++ SmartHubApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SmartHubApp
+//
+//  Created by Davide Di Domenico on 23/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SmartHubApp/SmartHubAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartHubAppApp.swift
+//  SmartHubApp
+//
+//  Created by Davide Di Domenico on 23/01/21.
+//
+
+import SwiftUI
+
+@main
+struct SmartHubAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
